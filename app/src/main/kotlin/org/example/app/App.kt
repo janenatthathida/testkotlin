d3df07743b0bba50d9f8b8d2dba06c648e082c39
K <K>
2024-05-02 12:20:24 +0000

    ok

--- app/src/main/kotlin/org/example/app/App.kt
+++ app/src/main/kotlin/org/example/app/App.kt
@@ -11,4 +11,6 @@ fun main() {
     val tokens = StringUtils.split(MessageUtils.getMessage())
     val result = StringUtils.join(tokens)
     println(WordUtils.capitalize(result))
+    println("ok")
+    
 }
